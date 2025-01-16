8bfb67aca57bc9bf2346f3834456efd119111dfa
Daniel Larva <daniel.larva8@gmail.com>
2025-01-16 19:10:33 +0200

    WORKING AS INTENDED WOOHOO

--- ImageRotatorScript.py
+++ ImageRotatorScript.py
@@ -1,7 +1,7 @@
-from PIL import Image
+from PIL import Image, ImageOps
 import os
 
-path = input('Please type in the name of the image\nfolder you want to work with: ')
+path = input('Please type in the name\nof the image folder you want to work with: ')
 path = './' + path
 
 angle = int(input('Is the "top" of the image facing left or right?\nEnter (1) for left\nEnter (2) for right\n'))
@@ -14,29 +14,16 @@ pathOut = './' + pathOut
 
 print('Thank you. One moment please.')
 
-if angle == 1: #Rotates the images clockwise by 90 degrees
-    for filename in os.listdir(path):
-        image = Image.open(f"{path}/{filename}")
-
-        rotation = image.transpose(Image.Transpose.ROTATE_270)
-        bareName = os.path.splitext(filename)[0]
-        print(bareName, 'rotated!')
-
-        try:
-            rotation.save(f'.{pathOut}/{bareName}_rotated.JPG')
-        except Exception:
-            os.mkdir(f'.{pathOut}')
-            rotation.save(f'.{pathOut}/{bareName}_rotated.JPG')
-#    rangle = 'ROTATE_90'
 for filename in os.listdir(path):
     if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
         image = Image.open(f'{path}/{filename}')
+        image = ImageOps.exif_transpose(image)
         bareName = os.path.splitext(filename)[0]
 
         if angle == 1:
-            rotation = image.rotate(270)
+            rotation = image.rotate(270, expand=True)
         else:
-            rotation = image.rotate(90)
+            rotation = image.rotate(90, expand=True)
         
         try:
             rotation.save(f'.{pathOut}/{bareName}_rotated.JPG')
@@ -45,20 +32,5 @@ for filename in os.listdir(path):
             rotation.save(f'.{pathOut}/{bareName}_rotated.JPG')
 
         print(bareName, 'done!')
-
-else: #Rotates the images counter-clockwise by 90 degrees
-    for filename in os.listdir(path):
-        image = Image.open(f"{path}/{filename}")
-
-        rotation = image.transpose(Image.Transpose.ROTATE_90)
-        bareName = os.path.splitext(filename)[0]
-        print(bareName, 'rotated!')
-
-        try:
-            rotation.save(f'.{pathOut}/{bareName}_rotated.JPG')
-        except Exception:
-            os.mkdir(f'.{pathOut}')
-            rotation.save(f'.{pathOut}/{bareName}_rotated.JPG')
-#    rangle = 'ROTATE_270'
 
 print('And we are done, please check the destination folder')
